ac10b5428e01ab7c46158cb6d7511ded0cee3111
talatkuyuk <talatkuyuk@gmail.com>
2025-01-31 15:52:05 +0300

    upgraded to motion from framer-motion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 
 import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 
